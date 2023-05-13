2af7bc440a9450f1f6277c5b4c3b541f088974c2
Jramongs18 <jramongs15@gmail.com>
2023-05-13 14:39:53 -0600

    Cambiando nombre de pestaña

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
+window.document.title='Formulario';
 
 function Formulario() {
   const [nombre, setNombre] = useState('');
